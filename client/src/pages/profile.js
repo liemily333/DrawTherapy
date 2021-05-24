4d4bd7f4f77b772fe55e3d2f53c68c96202a7868
Emily Lee <emlee088@gmail.com>
2021-05-24 13:25:33 -0700

    user profile

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "../components/navBar/navBar";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Axios from "axios";
+import axios from "axios";
 
 function userProfile() {
-  const getGames = () => {
-    Axios.get("http://localhost:3002/game", {
-      name1: name1,
-    }).then((response) => {
-      console.log(response);
-    });
-  };
+  // const [name1, setName1] = useState(null);
+  // const [name2, setName2] = useState(null);
+  // const [score1, setScore1] = useState(null);
+  // const [score2, setScore2] = useState(null);
+
+  const data = [];
+  console.log(data);
+
+  Axios.get("http://localhost:3002/games").then((response) => {
+    data.push(response);
+    console.log(data[0].data.data);
+  });
 
   return (
     <div>
@@ -20,10 +26,10 @@ function userProfile() {
       <div></div>
       <Container>
         <Row>
-          <Col>name1</Col>
-          <Col>name2</Col>
-          <Col>score1</Col>
-          <Col>score2</Col>
+          {/* <Col>{name1}</Col>
+          <Col>{name2}</Col>
+          <Col>{score1}</Col>
+          <Col>score2</Col> */}
           <Col>
             <button>delete</button>
           </Col>
@@ -32,5 +38,4 @@ function userProfile() {
     </div>
   );
 }
-
 export default userProfile;
