e4d354028d67e5d4634112a5cf12d432af5db9b3
Emily Lee <emlee088@gmail.com>
2021-07-08 11:22:45 -0700

    added canvas settings

--- client/src/pages/drawarea.js
+++ client/src/pages/drawarea.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useRef } from "react";
 import CanvasDraw from "react-canvas-draw";
 import rough from "roughjs";
 import "./style.css";
@@ -6,6 +6,28 @@ import "./style.css";
 function DrawArea() {
   const [radius, setRadius] = useState(null);
   const [color, setColor] = useState(null);
+
+  const savecanvas = useRef(null);
+  const savedcanvas = useRef(null);
+
+  const saveDrawing = () => {
+    console.log("click");
+    const data = savecanvas.current.getSaveData();
+    console.log("this is your drawing", data);
+  };
+
+  const SavedDrawing = () => {
+    const data = savecanvas.current.getSaveData();
+    savedcanvas.current.loadSaveData(data);
+  };
+
+  const clearCanvas = () => {
+    savecanvas.current.clear();
+  };
+
+  const undoCanvas = () => {
+    savecanvas.current.undo();
+  };
 
   function changeRadius() {
     setRadius(30);
@@ -22,9 +44,25 @@ function DrawArea() {
         canvasWidth={window.innerWidth}
         brushRadius={radius}
         brushColor={color}
+        getSaveData
+        ref={savecanvas}
+        hideGrid="true"
       />
       <button onClick={changeRadius}>30</button>
       <button onClick={changeColor}>pink</button>
+      <button onClick={clearCanvas}> clear canvas</button>
+      <button onClick={saveDrawing}> save drawing</button>
+      <button onClick={undoCanvas}>undo last move</button>
+
+      <CanvasDraw
+        canvasWidth={window.innerWidth}
+        brushRadius={radius}
+        brushColor={color}
+        getSaveData
+        ref={savedcanvas}
+        hideGrid="true"
+      />
+      <button onClick={SavedDrawing}>view drawing</button>
     </div>
   );
 }
